1dabbde01c91c58d5e6e19977210b7f89ca9c1d9
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-04-21 22:04:27 +0900

    add group

--- Gemfile
+++ Gemfile
@@ -54,4 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem 'font-awesome-sass'
+gem 'pry-rails'
 gem 'devise'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        
-  validates :name, presence: true, uniqueness: true
+         :recoverable, :rememberable,  :validatable
+
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
