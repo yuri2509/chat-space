26d5c4645743e98eb431cef065dabab6db422a35
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-04-26 14:35:05 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
 end
