19bdd70904df1f9c3bbe659e374e591946cb2676
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-05-09 00:46:35 +0900

    取得した投稿データを表示

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,13 +2,15 @@ $(function(){
     function buildHTML(message){
       if ( message.image ) {
         var html =
-          `<div class="main-ber__chat-spaces-space">
-            <p class="main-ber__chat-spaces-space-user">
-              ${message.user_name}
-            </p>
-            <p class="main-ber__chat-spaces-space-time">
-              ${message.created_at}
-            </p>
+          `<div class="main-ber__chat-spaces-space" data-message-id=${message.id}>
+            <div class="main-ber__chat-spaces-space">
+              <p class="main-ber__chat-spaces-space-user">
+                ${message.user_name}
+              </p>
+              <p class="main-ber__chat-spaces-space-time">
+                ${message.created_at}
+              </p>
+            </div>
           </div>
           <div class="main-ber__chat-spaces-lower">
             <p class="main-ber__chat-spaces-space-text">
@@ -19,14 +21,16 @@ $(function(){
         return html;
       } else {
         var html =
-          `<div class="main-ber__chat-spaces-space">
-            <p class="main-ber__chat-spaces-space-user">
-              ${message.user_name}
-            </p>
-            <p class="main-ber__chat-spaces-space-time">
-              ${message.created_at}
-            </p>
-          </div>
+          `<div class="main-ber__chat-spaces-space" data-message-id=${message.id}>
+            <div class="main-ber__chat-spaces-space">
+              <p class="main-ber__chat-spaces-space-user">
+                ${message.user_name}
+              </p>
+              <p class="main-ber__chat-spaces-space-time">
+                ${message.created_at}
+              </p>
+            </div>
+          <
           <div class="main-ber__chat-spaces-lower">
             <p class="main-ber__chat-spaces-space-text">
               ${message.content}
@@ -35,27 +39,47 @@ $(function(){
         return html;
       };
     }
-$('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.main-ber__chat-spaces').append(html);
-        $('form')[0].reset();
-        $('.form-btn').prop( 'disabled', false )
-        $('.main-ber__chat-spaces').animate({ scrollTop: $('.main-ber__chat-spaces')[0].scrollHeight});
+  $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
       })
-      .fail(function(data) {
-        alert("メッセージ送信に失敗しました");
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.main-ber__chat-spaces').append(html);
+          $('form')[0].reset();
+          $('.form-btn').prop( 'disabled', false )
+          $('.main-ber__chat-spaces').animate({ scrollTop: $('.main-ber__chat-spaces')[0].scrollHeight});
+        })
+        .fail(function(data) {
+          alert("メッセージ送信に失敗しました");
+      });
+  })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.main-ber__chat-spaces-space').append(insertHTML);
+    })
+    .fail(function() {
+      alert('error');
     });
-})
+  };
 });
