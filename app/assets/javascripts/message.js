409e9429a848e1d8d8ccd3bd96e2e8d0a52c6238
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-05-09 11:42:30 +0900

    メッセージを取得したら画面がスクロールする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -72,11 +72,14 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.main-ber__chat-spaces-space').append(insertHTML);
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-ber__chat-spaces-space').append(insertHTML);
+        $('.main-ber__chat-spaces-space').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      } 
     })
     .fail(function() {
       alert('error');
