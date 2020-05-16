2c91de44bebc9115fb08e7b93e306181e5e9d825
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-05-16 13:58:17 +0900

    ビューの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [400, 400]
+  process resize_to_fit: [200, 200]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
